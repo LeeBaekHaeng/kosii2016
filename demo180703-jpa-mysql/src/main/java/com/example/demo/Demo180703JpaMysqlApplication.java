29ed7d877934ad5fa790665ac43c2f10db5d2ce9
god <god@god-PC>
2018-07-03 14:55:11 +0900

    1

---
+++ demo180703-jpa-mysql/src/main/java/com/example/demo/Demo180703JpaMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo180703JpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo180703JpaMysqlApplication.class, args);
+	}
+}
